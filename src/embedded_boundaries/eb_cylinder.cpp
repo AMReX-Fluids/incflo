5636909873ace27dc869869c7c4d25038b6ea379
Oscar Antepara <oantepara@lbl.gov>
2020-08-07 12:42:38 -0700

    Adding additional 2d benchmarks tests (parallel planes and rotated planes)

--- src/embedded_boundaries/eb_cylinder.cpp
+++ src/embedded_boundaries/eb_cylinder.cpp
@@ -19,6 +19,8 @@ void incflo::make_eb_cylinder()
     Real radius = 0.0002;
     int direction = 0;
     Vector<Real> centervec(3);
+    Real rotation  = 0;
+    int rotation_axe  = 0;
 
     // Get cylinder information from inputs file.                               *
     ParmParse pp("cylinder");
@@ -26,22 +28,30 @@ void incflo::make_eb_cylinder()
     pp.query("internal_flow", inside);
     pp.query("radius", radius);
     pp.query("direction", direction);
+    pp.query("rotation",   rotation);
+    pp.query("rotation_axe",   rotation_axe);
     pp.getarr("center", centervec, 0, 3);
     Array<Real, AMREX_SPACEDIM> center = {AMREX_D_DECL(centervec[0], centervec[1], centervec[2])};
+
+    rotation = (rotation/180.)*M_PI;
 
     // Print info about cylinder
     amrex::Print() << " " << std::endl;
     amrex::Print() << " Internal Flow: " << inside << std::endl;
     amrex::Print() << " Radius:    " << radius << std::endl;
     amrex::Print() << " Direction: " << direction << std::endl;
+    amrex::Print() << " Rotation angle(rad): " << rotation << std::endl;
+    amrex::Print() << " Rotation axe: " << rotation_axe << std::endl;
     amrex::Print() << " Center:    " << center[0] << ", " << center[1] << ", " << center[2]
 				   << std::endl;
 
     // Build the Cylinder implficit function representing the curved walls     
     EB2::CylinderIF my_cyl(radius, direction, center, inside);
 
+    auto my_cyl_rot = EB2::rotate(my_cyl, rotation, rotation_axe);
+
     // Generate GeometryShop
-    auto gshop = EB2::makeShop(my_cyl);
+    auto gshop = EB2::makeShop(my_cyl_rot);
 
     // Build index space
     int max_level_here = 0;
