ce0e62251f7e06fe4911f8b788fcfdad2bcf2d71
Ann Almgren <asalmgren@lbl.gov>
2021-05-14 16:45:03 -0700

    Remember to initialize scratch to 1 in redistribute

--- src/convection/incflo_redistribute_convective_term.cpp
+++ src/convection/incflo_redistribute_convective_term.cpp
@@ -58,6 +58,15 @@ incflo::redistribute_convective_term ( Box const& bx, MFIter const& mfi,
         Array4<Real> scratch = scratch_fab.array();
         Elixir eli_scratch = scratch_fab.elixir();
 
+        // This is scratch space if calling StateRedistribute,
+        //  but is used as the weights (here set to 1) if calling
+        //  FluxRedistribute
+        amrex::ParallelFor(Box(scratch),
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+            {
+                scratch(i,j,k) = 1.;
+            });
+
         // velocity
         int ncomp = AMREX_SPACEDIM;
         auto const& bc_vel = get_velocity_bcrec_device_ptr();
