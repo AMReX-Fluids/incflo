735d40ec4b3f29d765f86230b36d61ea590191bf
Matthew Blomquist <blomquist.matt@gmail.com>
2022-07-06 15:11:50 -0700

    Create local copy of state redistributions.

---
+++ Redistribution/hydro_create_itracker_2d.cpp
@@ -0,0 +1,221 @@
+/**
+ * \file hydro_create_itracker_2d.cpp
+ * \addtogroup Redistribution
+ * @{
+ *
+ */
+
+#include <hydro_redistribution.H>
+
+using namespace amrex;
+
+#if (AMREX_SPACEDIM == 2)
+
+void
+Redistribution::MakeITracker ( Box const& bx,
+                               Array4<Real const> const& apx,
+                               Array4<Real const> const& apy,
+                               Array4<Real const> const& vfrac,
+                               Array4<int> const& itracker,
+                               Geometry const& lev_geom,
+                               Real target_volfrac)
+{
+#if 0
+    int debug_verbose = 0;
+#endif
+
+    const Real small_norm_diff = 1.e-8;
+
+    const Box domain = lev_geom.Domain();
+
+    // Note that itracker has 4 components and all are initialized to zero
+    // We will add to the first component every time this cell is included in a merged neighborhood,
+    //    either by merging or being merged
+    // We identify the cells in the remaining three components with the following ordering
+    //
+    // ^  6 7 8
+    // |  4   5
+    // j  1 2 3
+    //   i --->
+
+    Array<int,9> imap{0,-1,0,1,-1,1,-1,0,1};
+    Array<int,9> jmap{0,-1,-1,-1,0,0,1,1,1};
+
+    const auto& is_periodic_x = lev_geom.isPeriodic(0);
+    const auto& is_periodic_y = lev_geom.isPeriodic(1);
+
+//  if (debug_verbose > 0)
+//      amrex::Print() << " IN MAKE_ITRACKER DOING BOX " << bx << std::endl;
+
+    amrex::ParallelFor(Box(itracker),
+    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    {
+        itracker(i,j,k,0) = 0;
+    });
+
+    Box domain_per_grown = domain;
+    if (is_periodic_x) domain_per_grown.grow(0,4);
+    if (is_periodic_y) domain_per_grown.grow(1,4);
+
+    Box const& bxg4 = amrex::grow(bx,4);
+    Box bx_per_g4= domain_per_grown & bxg4;
+
+    amrex::ParallelFor(bx_per_g4,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    {
+       if (vfrac(i,j,k) > 0.0 && vfrac(i,j,k) < target_volfrac)
+       {
+           Real apnorm, apnorm_inv;
+           const Real dapx = apx(i+1,j  ,k  ) - apx(i,j,k);
+           const Real dapy = apy(i  ,j+1,k  ) - apy(i,j,k);
+           apnorm = std::sqrt(dapx*dapx+dapy*dapy);
+           apnorm_inv = 1.0/apnorm;
+           Real nx = dapx * apnorm_inv;
+           Real ny = dapy * apnorm_inv;
+
+           bool nx_eq_ny = ( (std::abs(nx-ny) < small_norm_diff) ||
+                             (std::abs(nx+ny) < small_norm_diff)  ) ? true : false;
+
+           // As a first pass, choose just based on the normal
+           if (std::abs(nx) > std::abs(ny))
+           {
+               if (nx > 0)
+                   itracker(i,j,k,1) = 5;
+               else
+                   itracker(i,j,k,1) = 4;
+
+           } else {
+               if (ny > 0)
+                   itracker(i,j,k,1) = 7;
+               else
+                   itracker(i,j,k,1) = 2;
+           }
+
+           bool xdir_mns_ok = (is_periodic_x || (i > domain.smallEnd(0)));
+           bool xdir_pls_ok = (is_periodic_x || (i < domain.bigEnd(0)  ));
+           bool ydir_mns_ok = (is_periodic_y || (j > domain.smallEnd(1)));
+           bool ydir_pls_ok = (is_periodic_y || (j < domain.bigEnd(1)  ));
+
+           // Override above logic if trying to reach outside a domain boundary (and non-periodic)
+           if ( (!xdir_mns_ok && (itracker(i,j,k,1) == 4)) ||
+                (!xdir_pls_ok && (itracker(i,j,k,1) == 5)) )
+           {
+               itracker(i,j,k,1) = (ny > 0) ? 7 : 2;
+           }
+           if ( (!ydir_mns_ok && (itracker(i,j,k,1) == 2)) ||
+                (!ydir_pls_ok && (itracker(i,j,k,1) == 7)) )
+           {
+               itracker(i,j,k,1) = (nx > 0) ? 5 : 4;
+           }
+
+           // (i,j) merges with at least one cell now
+           itracker(i,j,k,0) += 1;
+
+           // (i+ioff,j+joff) is in the nbhd of (i,j)
+           int ioff = imap[itracker(i,j,k,1)];
+           int joff = jmap[itracker(i,j,k,1)];
+
+           // Sanity check
+           if (vfrac(i+ioff,j+joff,k) == 0.)
+               amrex::Abort(" Trying to merge with covered cell");
+
+           Real sum_vol = vfrac(i,j,k) + vfrac(i+ioff,j+joff,k);
+
+#if 0
+           if (debug_verbose > 0)
+               amrex::Print() << "Cell " << IntVect(i,j) << " with volfrac " << vfrac(i,j,k) <<
+                                 " trying to merge with " << IntVect(i+ioff,j+joff) <<
+                                 " with volfrac " << vfrac(i+ioff,j+joff,k) <<
+                                 " to get new sum_vol " <<  sum_vol << std::endl;
+#endif
+
+           // If the merged cell isn't large enough, we try to merge in the other direction
+           if (sum_vol < target_volfrac || nx_eq_ny)
+           {
+               // Original offset was in y-direction, so we will add to the x-direction
+               // Note that if we can't because it would go outside the domain, we don't
+               if (ioff == 0) {
+                   if (nx >= 0 && xdir_pls_ok)
+                   {
+                       itracker(i,j,k,2) = 5;
+                       itracker(i,j,k,0) += 1;
+                   }
+                   else if (nx <= 0 && xdir_mns_ok)
+                   {
+                       itracker(i,j,k,2) = 4;
+                       itracker(i,j,k,0) += 1;
+                   }
+
+               // Original offset was in x-direction, so we will add to the y-direction
+               // Note that if we can't because it would go outside the domain, we don't
+               } else {
+                   if (ny >= 0 && ydir_pls_ok)
+                   {
+                       itracker(i,j,k,2) = 7;
+                       itracker(i,j,k,0) += 1;
+                   }
+                   else if (ny <= 0 && ydir_mns_ok)
+                   {
+                       itracker(i,j,k,2) = 2;
+                       itracker(i,j,k,0) += 1;
+                   }
+               }
+
+               if (itracker(i,j,k,0) > 1)
+               {
+                   // (i+ioff2,j+joff2) is in the nbhd of (i,j)
+                   int ioff2 = imap[itracker(i,j,k,2)];
+                   int joff2 = jmap[itracker(i,j,k,2)];
+
+                   sum_vol += vfrac(i+ioff2,j+joff2,k);
+#if 0
+                   if (debug_verbose > 0)
+                       amrex::Print() << "Cell " << IntVect(i,j) << " with volfrac " << vfrac(i,j,k) <<
+                                         " trying to ALSO merge with " << IntVect(i+ioff2,j+joff2) <<
+                                         " with volfrac " << vfrac(i+ioff2,j+joff2,k) <<
+                                          " to get new sum_vol " <<  sum_vol << std::endl;
+#endif
+               }
+           }
+
+           // Now we merge in the corner direction if we have already claimed two
+           if (itracker(i,j,k,0) == 2)
+           {
+               // We already have two offsets, and we know they are in different directions
+               ioff = imap[itracker(i,j,k,1)] + imap[itracker(i,j,k,2)];
+               joff = jmap[itracker(i,j,k,1)] + jmap[itracker(i,j,k,2)];
+
+               if (ioff > 0 && joff > 0)
+                   itracker(i,j,k,3) = 8;
+               else if (ioff < 0 && joff > 0)
+                   itracker(i,j,k,3) = 6;
+               else if (ioff > 0 && joff < 0)
+                   itracker(i,j,k,3) = 3;
+               else
+                   itracker(i,j,k,3) = 1;
+
+               // (i,j) merges with at least three cells now
+               itracker(i,j,k,0) += 1;
+
+               sum_vol += vfrac(i+ioff,j+joff,k);
+#if 0
+               if (debug_verbose > 0)
+                   amrex::Print() << "Cell " << IntVect(i,j) << " with volfrac " << vfrac(i,j,k) <<
+                                     " trying to ALSO merge with " << IntVect(i+ioff,j+joff) <<
+                                     " with volfrac " << vfrac(i+ioff,j+joff,k) <<
+                                     " to get new sum_vol " <<  sum_vol << std::endl;
+#endif
+           }
+           if (sum_vol < target_volfrac)
+           {
+#if 0
+             amrex::Print() << "Couldnt merge with enough cells to raise volume at " <<
+                               IntVect(i,j) << " so stuck with sum_vol " << sum_vol << std::endl;
+#endif
+             amrex::Abort("Couldnt merge with enough cells to raise volume greater than target_volfrac");
+           }
+       }
+    });
+}
+#endif
+/** @} */
---
+++ Redistribution/hydro_redistribution.cpp
@@ -0,0 +1,254 @@
+/**
+ * \file hydro_redistribution.cpp
+ * \addtogroup Redistribution
+ * @{
+ *
+ */
+
+#include <hydro_redistribution.H>
+#include <AMReX_EB_utils.H>
+
+using namespace amrex;
+
+void Redistribution::Apply ( Box const& bx, int ncomp,
+                             Array4<Real      > const& dUdt_out,
+                             Array4<Real      > const& dUdt_in,
+                             Array4<Real const> const& U_in,
+                             Array4<Real> const& scratch,
+                             Array4<EBCellFlag const> const& flag,
+                             AMREX_D_DECL(Array4<Real const> const& apx,
+                                          Array4<Real const> const& apy,
+                                          Array4<Real const> const& apz),
+                             Array4<amrex::Real const> const& vfrac,
+                             AMREX_D_DECL(Array4<Real const> const& fcx,
+                                          Array4<Real const> const& fcy,
+                                          Array4<Real const> const& fcz),
+                             Array4<Real const> const& ccc,
+                             amrex::BCRec  const* d_bcrec_ptr,
+                             Geometry const& lev_geom, Real dt,
+                             std::string redistribution_type,
+                             const int srd_max_order,
+                             amrex::Real target_volfrac,
+                             Array4<Real const> const& srd_update_scale)
+{
+    // redistribution_type = "NoRedist";       // no redistribution
+    // redistribution_type = "FluxRedist"      // flux_redistribute
+    // redistribution_type = "StateRedist";    // (weighted) state redistribute
+
+
+    // MATT -- Print Statement
+    amrex::Print() << "\n\n\n\n\n\nHEY CHECK THIS OUT!!!!!\n\n\n\n\n\n" << std::endl;
+
+    amrex::ParallelFor(bx,ncomp,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+        {
+            dUdt_out(i,j,k,n) = 0.;
+        });
+
+    if (redistribution_type == "FluxRedist")
+    {
+        int icomp = 0;
+        apply_flux_redistribution (bx, dUdt_out, dUdt_in, scratch, icomp, ncomp, flag, vfrac, lev_geom);
+
+    } else if (redistribution_type == "StateRedist") {
+
+        Box const& bxg1 = grow(bx,1);
+        Box const& bxg2 = grow(bx,2);
+        Box const& bxg3 = grow(bx,3);
+        Box const& bxg4 = grow(bx,4);
+
+#if (AMREX_SPACEDIM == 2)
+        // We assume that in 2D a cell will only need at most 3 neighbors to merge with, and we
+        //    use the first component of this for the number of neighbors
+        IArrayBox itracker(bxg4,4);
+        // How many nbhds is a cell in
+#else
+        // We assume that in 3D a cell will only need at most 7 neighbors to merge with, and we
+        //    use the first component of this for the number of neighbors
+        IArrayBox itracker(bxg4,8);
+#endif
+        FArrayBox nrs_fab(bxg3,1);
+        FArrayBox alpha_fab(bxg3,2);
+
+        // Total volume of all cells in my nbhd
+        FArrayBox nbhd_vol_fab(bxg2,1);
+
+        // Centroid of my nbhd
+        FArrayBox cent_hat_fab     (bxg3,AMREX_SPACEDIM);
+
+        Elixir eli_itr = itracker.elixir();
+        Array4<int> itr = itracker.array();
+        Array4<int const> itr_const = itracker.const_array();
+
+        Elixir eli_nrs = nrs_fab.elixir();
+        Array4<Real      > nrs       = nrs_fab.array();
+        Array4<Real const> nrs_const = nrs_fab.const_array();
+
+        Elixir eli_alpha = alpha_fab.elixir();
+        Array4<Real      > alpha       = alpha_fab.array();
+        Array4<Real const> alpha_const = alpha_fab.const_array();
+
+        Elixir eli_nbf = nbhd_vol_fab.elixir();
+        Array4<Real      > nbhd_vol       = nbhd_vol_fab.array();
+        Array4<Real const> nbhd_vol_const = nbhd_vol_fab.const_array();
+
+        Elixir eli_chf = cent_hat_fab.elixir();
+        Array4<Real      > cent_hat       = cent_hat_fab.array();
+        Array4<Real const> cent_hat_const = cent_hat_fab.const_array();
+
+        Box domain_per_grown = lev_geom.Domain();
+        AMREX_D_TERM(if (lev_geom.isPeriodic(0)) domain_per_grown.grow(0,1);,
+                     if (lev_geom.isPeriodic(1)) domain_per_grown.grow(1,1);,
+                     if (lev_geom.isPeriodic(2)) domain_per_grown.grow(2,1););
+
+        // At any external Dirichlet domain boundaries we need to set dUdt_in to 0
+        //    in the cells just outside the domain because those values will be used
+        //    in the slope computation in state redistribution.  We assume here that
+        //    the ext_dir values of U_in itself have already been set.
+        if (!domain_per_grown.contains(bxg1))
+            amrex::ParallelFor(bxg1,ncomp,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+                {
+                    if (!domain_per_grown.contains(IntVect(AMREX_D_DECL(i,j,k))))
+                        dUdt_in(i,j,k,n) = 0.;
+                });
+
+        amrex::ParallelFor(Box(scratch), ncomp,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+            {
+                const Real scale = (srd_update_scale) ? srd_update_scale(i,j,k) : Real(1.0);
+                scratch(i,j,k,n) = U_in(i,j,k,n) + dt * dUdt_in(i,j,k,n) / scale;
+            }
+        );
+
+        MakeITracker(bx, AMREX_D_DECL(apx, apy, apz), vfrac, itr, lev_geom, target_volfrac);
+
+        MakeStateRedistUtils(bx, flag, vfrac, ccc, itr, nrs, alpha, nbhd_vol, cent_hat,
+                             lev_geom, target_volfrac);
+
+        StateRedistribute(bx, ncomp, dUdt_out, scratch, flag, vfrac,
+                          AMREX_D_DECL(fcx, fcy, fcz), ccc,  d_bcrec_ptr,
+                          itr_const, nrs_const, alpha_const, nbhd_vol_const,
+                          cent_hat_const, lev_geom, srd_max_order);
+
+        amrex::ParallelFor(bx, ncomp,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+            {
+                // Only update the values which actually changed -- this makes
+                // the results insensitive to tiling -- otherwise cells that aren't
+                // changed but are in a tile on which StateRedistribute gets called
+                // will have precision-level changes due to adding/subtracting U_in
+                // and multiplying/dividing by dt.   Here we test on whether (i,j,k)
+                // has at least one neighbor and/or whether (i,j,k) is in the
+                // neighborhood of another cell -- if either of those is true the
+                // value may have changed
+
+                if (itr(i,j,k,0) > 0 || nrs(i,j,k) > 1.)
+                {
+                   const Real scale = (srd_update_scale) ? srd_update_scale(i,j,k) : Real(1.0);
+
+                   dUdt_out(i,j,k,n) = scale * (dUdt_out(i,j,k,n) - U_in(i,j,k,n)) / dt;
+
+                }
+                else
+                {
+                   dUdt_out(i,j,k,n) = dUdt_in(i,j,k,n);
+                }
+            }
+        );
+
+    } else if (redistribution_type == "NoRedist") {
+        amrex::ParallelFor(bx, ncomp,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+            {
+                dUdt_out(i,j,k,n) = dUdt_in(i,j,k,n);
+            }
+        );
+
+    } else {
+       amrex::Error("Not a legit redist_type");
+    }
+}
+
+void
+Redistribution::ApplyToInitialData ( Box const& bx, int ncomp,
+                                     Array4<Real      > const& U_out,
+                                     Array4<Real      > const& U_in,
+                                     Array4<EBCellFlag const> const& flag,
+                                     AMREX_D_DECL(amrex::Array4<amrex::Real const> const& apx,
+                                                  amrex::Array4<amrex::Real const> const& apy,
+                                                  amrex::Array4<amrex::Real const> const& apz),
+                                     amrex::Array4<amrex::Real const> const& vfrac,
+                                     AMREX_D_DECL(amrex::Array4<amrex::Real const> const& fcx,
+                                                  amrex::Array4<amrex::Real const> const& fcy,
+                                                  amrex::Array4<amrex::Real const> const& fcz),
+                                     amrex::Array4<amrex::Real const> const& ccc,
+                                     amrex::BCRec  const* d_bcrec_ptr,
+                                     Geometry& lev_geom, std::string redistribution_type,
+                                     const int srd_max_order,
+                                     amrex::Real target_volfrac)
+{
+    if (redistribution_type != "StateRedist") {
+    std::string msg = "Redistribution::ApplyToInitialData: Shouldn't be here with redist type "+redistribution_type;
+        amrex::Error(msg);
+    }
+
+    Box const& bxg2 = grow(bx,2);
+    Box const& bxg3 = grow(bx,3);
+    Box const& bxg4 = grow(bx,4);
+
+#if (AMREX_SPACEDIM == 2)
+    // We assume that in 2D a cell will only need at most 3 neighbors to merge with, and we
+    //    use the first component of this for the number of neighbors
+    IArrayBox itracker(bxg4,4);
+#else
+    // We assume that in 3D a cell will only need at most 7 neighbors to merge with, and we
+    //    use the first component of this for the number of neighbors
+    IArrayBox itracker(bxg4,8);
+#endif
+    FArrayBox nrs_fab(bxg3,1);
+    FArrayBox alpha_fab(bxg3,2);
+
+    // Total volume of all cells in my nbhd
+    FArrayBox nbhd_vol_fab(bxg2,1);
+
+    // Centroid of my nbhd
+    FArrayBox cent_hat_fab  (bxg3,AMREX_SPACEDIM);
+
+    Elixir eli_itr = itracker.elixir();
+    Array4<int> itr = itracker.array();
+    Array4<int const> itr_const = itracker.const_array();
+
+    Elixir eli_nrs = nrs_fab.elixir();
+    Array4<Real      > nrs       = nrs_fab.array();
+    Array4<Real const> nrs_const = nrs_fab.const_array();
+
+    Elixir eli_alpha = alpha_fab.elixir();
+    Array4<Real      > alpha       = alpha_fab.array();
+    Array4<Real const> alpha_const = alpha_fab.const_array();
+
+    Elixir eli_nbf = nbhd_vol_fab.elixir();
+    Array4<Real      > nbhd_vol       = nbhd_vol_fab.array();
+    Array4<Real const> nbhd_vol_const = nbhd_vol_fab.const_array();
+
+    Elixir eli_chf = cent_hat_fab.elixir();
+    Array4<Real      > cent_hat       = cent_hat_fab.array();
+    Array4<Real const> cent_hat_const = cent_hat_fab.const_array();
+
+    amrex::ParallelFor(bx,ncomp,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+    {
+        U_out(i,j,k,n) = 0.;
+    });
+
+    MakeITracker(bx, AMREX_D_DECL(apx, apy, apz), vfrac, itr, lev_geom, target_volfrac);
+
+    MakeStateRedistUtils(bx, flag, vfrac, ccc, itr, nrs, alpha, nbhd_vol, cent_hat,
+                            lev_geom, target_volfrac);
+
+    StateRedistribute(bx, ncomp, U_out, U_in, flag, vfrac,
+                         AMREX_D_DECL(fcx, fcy, fcz), ccc,  d_bcrec_ptr,
+                      itr_const, nrs_const, alpha_const, nbhd_vol_const,
+                      cent_hat_const, lev_geom, srd_max_order);
+}
+/** @} */
