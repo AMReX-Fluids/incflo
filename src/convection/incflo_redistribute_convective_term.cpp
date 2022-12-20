412bb2d414963b37b79995ad32b2bc52a45d1158
cgilet <cgilet@gmail.com>
2022-12-20 11:58:08 -0500

    Redistribution needs face centroids at new time. Want to use the nbdh polynomial to put the solution at the new time location. This change allows us to use slopes and maintain conservation.

--- src/convection/incflo_redistribute_convective_term.cpp
+++ src/convection/incflo_redistribute_convective_term.cpp
@@ -41,9 +41,9 @@ incflo::redistribute_convective_term ( Box const& bx, MFIter const& mfi,
 
     if (!regular)
     {
-        AMREX_D_TERM(fcx = ebfact_old->getFaceCent()[0]->const_array(mfi);,
-                     fcy = ebfact_old->getFaceCent()[1]->const_array(mfi);,
-                     fcz = ebfact_old->getFaceCent()[2]->const_array(mfi););
+        AMREX_D_TERM(fcx = ebfact_new->getFaceCent()[0]->const_array(mfi);,
+                     fcy = ebfact_new->getFaceCent()[1]->const_array(mfi);,
+                     fcz = ebfact_new->getFaceCent()[2]->const_array(mfi););
         ccc   = ebfact_old->getCentroid().const_array(mfi);
         AMREX_D_TERM(apx_old = ebfact_old->getAreaFrac()[0]->const_array(mfi);,
                      apy_old = ebfact_old->getAreaFrac()[1]->const_array(mfi);,
