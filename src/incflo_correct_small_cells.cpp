72330503c1ddb0a3ebe3724359aef32cdb69afca
Deepak Rangarajan <deepak.rangarajan@netl.doe.gov>
2023-01-13 21:26:27 +0000

    add special handling inside incflo_correct_small_cells for the case with flow through EB

--- src/incflo_correct_small_cells.cpp
+++ src/incflo_correct_small_cells.cpp
@@ -48,26 +48,86 @@ incflo::incflo_correct_small_cells (Vector<MultiFab*      > const& vel_in,
 
              const auto& ccvel_fab = vel_in[lev]->array(mfi);
 
-             // This FAB has cut cells -- we define the centroid value in terms of the MAC velocities onfaces
-             amrex::ParallelFor(bx,
-               [vfrac_fab,AMREX_D_DECL(apx_fab,apy_fab,apz_fab),ccvel_fab,AMREX_D_DECL(umac_fab,vmac_fab,wmac_fab)]
-               AMREX_GPU_DEVICE (int i, int j, int k) noexcept
-             {
-                 if (vfrac_fab(i,j,k) > 0.0 && vfrac_fab(i,j,k) < 1.e-4)
-                 {
-                    AMREX_D_TERM(Real u_avg = (apx_fab(i,j,k) * umac_fab(i,j,k) + apx_fab(i+1,j,k) * umac_fab(i+1,j,k))
-                                            / (apx_fab(i,j,k) + apx_fab(i+1,j,k));,
-                                 Real v_avg = (apy_fab(i,j,k) * vmac_fab(i,j,k) + apy_fab(i,j+1,k) * vmac_fab(i,j+1,k))
-                                            / (apy_fab(i,j,k) + apy_fab(i,j+1,k));,
-                                 Real w_avg = (apz_fab(i,j,k) * wmac_fab(i,j,k) + apz_fab(i,j,k+1) * wmac_fab(i,j,k+1))
-                                            / (apz_fab(i,j,k) + apz_fab(i,j,k+1)););
+             if (!m_eb_flow.enabled) {
+                // This FAB has cut cells -- we define the centroid value in terms of the MAC velocities onfaces
+                amrex::ParallelFor(bx,
+                  [vfrac_fab,AMREX_D_DECL(apx_fab,apy_fab,apz_fab),ccvel_fab,AMREX_D_DECL(umac_fab,vmac_fab,wmac_fab)]
+                  AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+                {
+                    if (vfrac_fab(i,j,k) > 0.0 && vfrac_fab(i,j,k) < 1.e-4)
+                    {
+                       AMREX_D_TERM(Real u_avg = (apx_fab(i,j,k) * umac_fab(i,j,k) + apx_fab(i+1,j,k) * umac_fab(i+1,j,k))
+                                               / (apx_fab(i,j,k) + apx_fab(i+1,j,k));,
+                                    Real v_avg = (apy_fab(i,j,k) * vmac_fab(i,j,k) + apy_fab(i,j+1,k) * vmac_fab(i,j+1,k))
+                                               / (apy_fab(i,j,k) + apy_fab(i,j+1,k));,
+                                    Real w_avg = (apz_fab(i,j,k) * wmac_fab(i,j,k) + apz_fab(i,j,k+1) * wmac_fab(i,j,k+1))
+                                               / (apz_fab(i,j,k) + apz_fab(i,j,k+1)););
 
-                    AMREX_D_TERM(ccvel_fab(i,j,k,0) = u_avg;,
-                                 ccvel_fab(i,j,k,1) = v_avg;,
-                                 ccvel_fab(i,j,k,2) = w_avg;);
+                       AMREX_D_TERM(ccvel_fab(i,j,k,0) = u_avg;,
+                                    ccvel_fab(i,j,k,1) = v_avg;,
+                                    ccvel_fab(i,j,k,2) = w_avg;);
 
-                 }
-             });
+                    }
+                });
+             } else { // EB has flow
+                Array4<Real const> const& eb_vel = get_velocity_eb()[lev]->const_array(mfi);
+                Array4<Real const> const& barea = EBFactory(lev).getBndryArea().const_array(mfi);
+
+                // This FAB has cut cells -- we define the centroid value in terms of the MAC velocities onfaces
+                amrex::ParallelFor(bx,
+                  [vfrac_fab,AMREX_D_DECL(apx_fab,apy_fab,apz_fab),ccvel_fab,AMREX_D_DECL(umac_fab,vmac_fab,wmac_fab),eb_vel]
+                  AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+                {
+                    if (vfrac_fab(i,j,k) > 0.0 && vfrac_fab(i,j,k) < 1.e-4)
+                    {
+                       AMREX_D_TERM(const Real ucc = ccvel_fab(i,j,k,0);,
+                                    const Real vcc = ccvel_fab(i,j,k,1);,
+                                    const Real wcc = ccvel_fab(i,j,k,2););
+
+                       AMREX_D_TERM(Real u_avg = (apx_fab(i,j,k) * umac_fab(i,j,k) + apx_fab(i+1,j,k) * umac_fab(i+1,j,k))
+                                               / (apx_fab(i,j,k) + apx_fab(i+1,j,k));,
+                                    Real v_avg = (apy_fab(i,j,k) * vmac_fab(i,j,k) + apy_fab(i,j+1,k) * vmac_fab(i,j+1,k))
+                                               / (apy_fab(i,j,k) + apy_fab(i,j+1,k));,
+                                    Real w_avg = (apz_fab(i,j,k) * wmac_fab(i,j,k) + apz_fab(i,j,k+1) * wmac_fab(i,j,k+1))
+                                               / (apz_fab(i,j,k) + apz_fab(i,j,k+1)););
+
+
+                       AMREX_D_TERM(ccvel_fab(i,j,k,0) = u_avg;,
+                                    ccvel_fab(i,j,k,1) = v_avg;,
+                                    ccvel_fab(i,j,k,2) = w_avg;);
+
+                       AMREX_D_TERM(const Real u_eb = eb_vel(i,j,k,0);,
+                                    const Real v_eb = eb_vel(i,j,k,1);,
+                                    const Real w_eb = eb_vel(i,j,k,2););
+#if (AMREX_SPACEDIM == 2)
+                       const Real eb_vel_mag = std::sqrt(u_eb*u_eb + v_eb*v_eb);
+#elif (AMREX_SPACEDIM == 3)
+                       const Real eb_vel_mag = std::sqrt(u_eb*u_eb + v_eb*v_eb + w_eb*w_eb);
+#endif
+                       constexpr amrex::Real tolerance = std::numeric_limits<amrex::Real>::epsilon();
+
+                       if (eb_vel_mag > tolerance) {
+                          if ( u_eb > tolerance ) {
+                              ccvel_fab(i,j,k,0) = amrex::min(ucc, u_eb);
+                          } else if( u_eb < -tolerance) {
+                              ccvel_fab(i,j,k,0) = amrex::max(ucc, u_eb);
+                          }
+                          if ( v_eb > tolerance ) {
+                              ccvel_fab(i,j,k,1) = amrex::min(vcc, v_eb);
+                          } else if( v_eb < -tolerance) {
+                              ccvel_fab(i,j,k,1) = amrex::max(vcc, v_eb);
+                          }
+#if (AMREX_SPACEDIM == 3)
+                          if ( w_eb > tolerance ) {
+                              ccvel_fab(i,j,k,2) = amrex::min(wcc, w_eb);
+                          } else if( w_eb < -tolerance) {
+                              ccvel_fab(i,j,k,2) = amrex::max(wcc, w_eb);
+                          }
+#endif
+                       }
+                    }
+                });
+             }
           }
        }
     }
