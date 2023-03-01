233bad5cd71fcfc69bac410b18da6622b5c5d0aa
cgilet <cgilet@gmail.com>
2023-03-01 10:33:44 -0500

    Enure we don't double include cells while enforcing reciprocity.

--- src/redistribution/hydro_create_itracker_2d.cpp
+++ src/redistribution/hydro_create_itracker_2d.cpp
@@ -367,56 +367,94 @@ newlyUncoveredNbhd ( int i, int j,
 //
 // Central Merging
 //
+void
+centralMerging ( int i, int j,
+                 Array4<Real const> const& apx,
+                 Array4<Real const> const& apy,
+                 Array4<Real const> const& vfrac,
+                 Array4<int> const& itracker,
+                 Geometry const& lev_geom,
+                 Real target_volfrac)
+{
+    const Box domain = lev_geom.Domain();
+    const auto& is_periodic_x = lev_geom.isPeriodic(0);
+    const auto& is_periodic_y = lev_geom.isPeriodic(1);
+    int k=0, kk=0;
+    Real sum_vol = vfrac(i,j,k);
 
-//            Real sum_vol = vfrac_new(i,j,k);
+    //int label[3][3] = {{1,2,3}, {4,0,5}, {6,7,8}};
+    int label[3][3] = {{1,4,6}, {2,0,7}, {3,5,8}};
+    for(int jj(-1); jj<=1; jj++) {
+        for(int ii(-1); ii<=1; ii++) {
+            if (vfrac(i+ii,j+jj,k+kk) > 0.0)
+            {
+                if ( !(ii==0 && jj==0)
+                     &&(is_periodic_x || (domain.smallEnd(0)<=i+ii && i+ii<=domain.bigEnd(0)))
+                     &&(is_periodic_y || (domain.smallEnd(1)<=j+jj && j+jj<=domain.bigEnd(1))))
+                {
+                    // take all valid interior cells in 3x3 neighborhood centered
+                    // excluding i,j itself
+                    // this includes any newly uncovered cells
+                    itracker(i,j,k,0) += 1;
+                    itracker(i,j,k,itracker(i,j,k,0)) = label[ii+1][jj+1];
+                    sum_vol += vfrac(i+ii,j+jj,k+kk);
 
-//            //int label[3][3] = {{1,2,3}, {4,0,5}, {6,7,8}};
-//         int label[3][3] = {{1,4,6}, {2,0,7}, {3,5,8}};
-//         int kk=0;
-//         for(int jj(-1); jj<=1; jj++) {
-//             for(int ii(-1); ii<=1; ii++) {
-//                 if (flag_new(i,j,k).isConnected(ii,jj,kk))
-//                 {
-//                     if ( !(ii==0 && jj==0)
-//                         &&(is_periodic_x || (domain.smallEnd(0)<=i+ii && i+ii<=domain.bigEnd(0)))
-//                         &&(is_periodic_y || (domain.smallEnd(1)<=j+jj && j+jj<=domain.bigEnd(1))))
-//                     {
-//                         // take all valid interior cells in 3x3 neighborhood centered
-//                         // excluding i,j itself
-//                         // this includes any newly uncovered cells
-//                         itracker(i,j,k,0) += 1;
-//                         itracker(i,j,k,itracker(i,j,k,0)) = label[ii+1][jj+1];
-//                         sum_vol += vfrac_new(i+ii,j+jj,k);
-
-//                         //FIXME -- still need to enfore reciprocity for newly uncovered cells
-// //fixme
-//                         if ( i==16 && j==8 ){
-//                             Print()<<"Including cell ("<<ii<<","<<jj<<"). label: "
-//                                    <<label[ii+1][jj+1]<<std::endl;
-//                         }
-//                     }
-//                 }
-//                 //FIXME - do we really want to try to include newly covered cells here?
-//                 // they can't fully participate in SRD because we don't give them a nbhd...
-//                 else if (flag_old(i,j,k).isConnected(ii,jj,kk))
-//                 {
-//                        // Add newly covered cells to the neighborhood
-//                     if (  (is_periodic_x || (domain.smallEnd(0)<=i+ii && i+ii<=domain.bigEnd(0)))
-//                         &&(is_periodic_y || (domain.smallEnd(1)<=j+jj && j+jj<=domain.bigEnd(1))))
-//                     {
-//                         itracker(i,j,k,0) += 1;
-//                         itracker(i,j,k,itracker(i,j,k,0)) = label[ii+1][jj+1];
-//                         sum_vol += vfrac_new(i+ii,j+jj,k);
-// //fixme
-//                         if ( i==16 && j==8 ){
-//                             Print()<<"Including newly covered cell ("<<ii<<","<<jj<<"). label: "
-//                                    <<label[ii+1][jj+1]<<std::endl;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
+                    //FIXME -- still need to enfore reciprocity for newly uncovered cells
+//fixme
+                    if ( i==16 && j==8 ){
+                        Print()<<"Including cell ("<<ii<<","<<jj<<"). label: "
+                               <<label[ii+1][jj+1]<<std::endl;
+                    }
+                }
+            }
+        }
+    }
 // Add check on sum_vol...
+}
+
+void
+enforceReciprocity(int i, int j, int k, Array4<int> const& itracker)
+{
+    auto map = Redistribution::getCellMap();
+    // Inverse map
+    auto nmap = Redistribution::getInvCellMap();
+
+    // Loop over my neighbors to make sure it's reciprocal, i.e. that my neighbor
+    // include me in thier neighborhood too.
+    for (int i_nbor = 1; i_nbor <= itracker(i,j,k,0); i_nbor++)
+    {
+        int ioff = map[0][itracker(i,j,k,i_nbor)];
+        int joff = map[1][itracker(i,j,k,i_nbor)];
+        int koff = (AMREX_SPACEDIM < 3) ? 0 : map[2][itracker(i,j,k,i_nbor)];
+
+        int ii = i+ioff;
+        int jj = j+joff;
+        int kk = k+koff;
+
+        if ( Box(itracker).contains(Dim3{ii,jj,kk}) )
+        {
+            int nbor = itracker(i,j,k,i_nbor);
+            int me = nmap[nbor];
+            bool found = false;
+
+            // Loop over the neighbor's neighbors to see if I'm already included
+            // If not, add me to the neighbor list.
+            for (int i_nbor2 = 1; i_nbor2 <= itracker(ii,jj,kk,0); i_nbor2++)
+            {
+                if ( itracker(ii,jj,kk,i_nbor2) == me ) {
+                    Print()<<IntVect(i,j)<<" is ALREADY A NEIGHBOR!"<<std::endl;
+                    found = true;
+                    break;
+                }
+            }
+            if ( !found )
+            {
+                itracker(ii,jj,kk,0) += 1;
+                itracker(ii,jj,kk,itracker(ii,jj,kk,0)) = me;
+            }
+        }
+    }
+}
 
 void
 Redistribution::MakeITracker ( Box const& bx,
@@ -433,6 +471,7 @@ Redistribution::MakeITracker ( Box const& bx,
 {
     int debug_verbose = 1;
 
+    // Map used for printing neighborhoods if desired
     auto map = getCellMap();
     Array<int,9> imap = map[0];
     Array<int,9> jmap = map[1];
@@ -515,43 +554,14 @@ Redistribution::MakeITracker ( Box const& bx,
     amrex::Print() << std::endl;
 #endif
 
-    // FIXME - Need to do some sort of check for if they've already been included though
     // Check uncovered and covered cells, make sure the neighbors also include them.
     amrex::ParallelFor(Box(itracker),
     [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
-        // Newly covered Cells
-        if (vfrac_new(i,j,k) == 0. && vfrac_old(i,j,k) > 0.0)
+        if ( (vfrac_new(i,j,k) > 0. && vfrac_new(i,j,k) < 1. && vfrac_old(i,j,k) == 0.0 ) // Newly uncovered
+             || (vfrac_new(i,j,k) == 0. && vfrac_old(i,j,k) > 0.0) ) // Newly covered Cells
         {
-            for (int i_nbor = 1; i_nbor <= itracker(i,j,k,0); i_nbor++)
-            {
-                int ioff = imap[itracker(i,j,k,i_nbor)];
-                int joff = jmap[itracker(i,j,k,i_nbor)];
-
-                if ( Box(itracker).contains(IntVect(i+ioff,j+joff)) )
-                {
-                    // amrex::Print() << "Cell  " << IntVect(i,j) << " is covered and merged with neighbor at " << IntVect(i+ioff,j+joff) << std::endl;
-                    itracker(i+ioff,j+joff,k,0) += 1;
-                    itracker(i+ioff,j+joff,k,itracker(i+ioff,j+joff,k,0)) = nmap[itracker(i,j,k,i_nbor)];
-                }
-            }
-        }
-
-        // Newly uncovered
-        if (vfrac_new(i,j,k) > 0. && vfrac_new(i,j,k) < 1. && vfrac_old(i,j,k) == 0.0 )
-        {
-            for (int i_nbor = 1; i_nbor <= itracker(i,j,k,0); i_nbor++)
-            {
-                int ioff = imap[itracker(i,j,k,i_nbor)];
-                int joff = jmap[itracker(i,j,k,i_nbor)];
-
-                if ( Box(itracker).contains(IntVect(i+ioff,j+joff)) )
-                {
-                    // amrex::Print() << "Cell  " << IntVect(i,j) << " is newly uncovered and merged with neighbor at " << IntVect(i+ioff,j+joff) << std::endl;
-                    itracker(i+ioff,j+joff,k,0) += 1;
-                    itracker(i+ioff,j+joff,k,itracker(i+ioff,j+joff,k,0)) = nmap[itracker(i,j,k,i_nbor)];
-                }
-            }
+            enforceReciprocity(i, j, k, itracker);
         }
     });
 
