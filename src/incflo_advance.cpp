6ec7a01aeff59d00792b994c70db290e80cf9cff
cgilet <cgilet@gmail.com>
2023-03-24 14:40:49 -0400

    Make sure to fill gradp newly uncovered cells

--- src/incflo_advance.cpp
+++ src/incflo_advance.cpp
@@ -124,6 +124,11 @@ void incflo::Advance()
             fillpatch_tracer(lev, m_t_old[lev], m_leveldata[lev]->tracer_o, ng);
         }
 
+	Redistribution::FillNewlyUncovered(m_leveldata[lev]->gp,
+                                           OldEBFactory(lev), EBFactory(lev),
+                                           *get_velocity_eb()[lev],
+                                           geom[lev], target_volfrac);
+
 
 // // Fill cells that were previously covered but become cut cell with the average
         // // of it's neighbors for now. (Later we want to use it's SRD merging nbhd)
@@ -141,7 +146,7 @@ void incflo::Advance()
 
         //FIXME - will need to be more careful here when adding diffusion since
         // divtau hasn't been computed yet. Likely want to do this after diffusion solve.
-        m_leveldata[lev]->divtau_o.setVal(0.0);
+        //m_leveldata[lev]->divtau_o.setVal(0.0);
 
 // FIXME will we also need to worry about all the pieces of U*, forces, etc???
     }
@@ -167,7 +172,7 @@ void incflo::Advance()
 
         //FIXME
     // this will overwrite the previous time plotfile
-    // WritePlotFile();
+//	WritePlotFile();
     // static int count=0; count++;
     //if (count>2) Abort();
 
