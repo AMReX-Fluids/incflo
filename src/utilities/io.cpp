d7d7ea809d00015a81c2001c1267544f8f21e16c
mblomquist <mblomquist@ucmerced.edu>
2023-04-09 15:42:46 -0700

    Update inputs_osc_cyl to use CFL. Newly uncovered error happens at time step 13. Add drangara drag computation.

--- src/utilities/io.cpp
+++ src/utilities/io.cpp
@@ -649,3 +649,116 @@ void incflo::WritePlotFile()
                                    pltscaVarsName, Geom(), m_cur_time, istep, refRatio());
     WriteJobInfo(plotfilename);
 }
+
+
+// Copy/Paste drag computation from from drangara
+/*
+void incflo::PrintDragForce() {
+   Print() << "PrintDragForce\n";
+
+   Vector<MultiFab> gradx(finest_level + 1);
+   Vector<MultiFab> grady(finest_level + 1);
+   Vector<MultiFab> gradeb(finest_level + 1);
+   for (int lev = 0; lev <= finest_level; ++lev) {
+       gradx[lev].define(grids[lev], dmap[lev], 2, 0, MFInfo(), Factory(lev));
+       grady[lev].define(grids[lev], dmap[lev], 2, 0, MFInfo(), Factory(lev));
+       gradeb[lev].define(grids[lev], dmap[lev], 2, 0, MFInfo(), Factory(lev));
+       gradx[lev].setVal(0.);
+       grady[lev].setVal(0.);
+       gradeb[lev].setVal(0.);
+   }
+
+   for (int lev = 0; lev <= finest_level; lev++)
+   {
+      auto& ld = *m_leveldata[lev];
+
+      for (MFIter mfi(ld.velocity,TilingIfNotGPU()); mfi.isValid(); ++mfi)
+      {
+         const Box& bx = mfi.validbox();
+         auto const& fact = EBFactory(lev);
+         int ncomp = ld.velocity.nComp();
+
+         Array4<Real> const& gradx_arr         = gradx[lev].array(mfi);
+         Array4<Real> const& grady_arr         = grady[lev].array(mfi);
+         Array4<Real> const& gradeb_arr        = gradeb[lev].array(mfi);
+         Array4<const Real> const& phi_arr     = ld.velocity.array(mfi);
+         Array4<const Real> const& phi_eb_arr  = get_velocity_eb()[lev]->array(mfi);
+
+         Array4<Real const> const& fcx   = (fact.getFaceCent())[0]->const_array(mfi);
+         Array4<Real const> const& fcy   = (fact.getFaceCent())[1]->const_array(mfi);
+ #if (AMREX_SPACEDIM == 3)
+         Array4<Real const> const& fcz   = (fact.getFaceCent())[2]->const_array(mfi);
+ #endif
+         Array4<Real const> const& ccent = (fact.getCentroid()).array(mfi);
+         Array4<Real const> const& bcent = (fact.getBndryCent()).array(mfi);
+         Array4<Real const> const& bnorm = (fact.getBndryNormal()).array(mfi);
+         const FabArray<EBCellFlagFab>* flags = &(fact.getMultiEBCellFlagFab());
+         Array4<EBCellFlag const> const& flag = flags->const_array(mfi);
+
+
+         bool is_eb_dirichlet = true;
+         bool is_eb_inhomog  = true;
+
+         amrex::ParallelFor(bx, ncomp,
+         [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+         {
+            if (flag(i,j,k).isSingleValued()) {
+               Real yloc_on_xface = fcx(i,j,k,0);
+               Real xloc_on_yface = fcy(i,j,k,0);
+               Real nx = bnorm(i,j,k,0);
+               Real ny = bnorm(i,j,k,1);
+#if (AMREX_SPACEDIM == 2)
+               gradx_arr(i,j,k,n) = amrex::grad_x_of_phi_on_centroids(i,j,k,n,
+                                                                      phi_arr,phi_eb_arr,
+                                                                      flag,ccent,bcent,
+                                                                      yloc_on_xface,
+                                                                      is_eb_dirichlet,is_eb_inhomog);
+
+               grady_arr(i,j,k,n) = amrex::grad_y_of_phi_on_centroids(i,j,k,n,
+                                                                      phi_arr,phi_eb_arr,
+                                                                      flag,ccent,bcent,
+                                                                      xloc_on_yface,
+                                                                      is_eb_dirichlet,is_eb_inhomog);
+
+               gradeb_arr(i,j,k,n) = amrex::grad_eb_of_phi_on_centroids(i,j,k,n,
+                                                                        phi_arr,phi_eb_arr,
+                                                                        flag,ccent,bcent,
+                                                                        nx, ny, is_eb_inhomog);
+
+#endif
+            }
+         });
+      }
+   }
+
+   Vector<int> istep(finest_level + 1, m_nstep);
+   Vector<MultiFab> plotmf(finest_level + 1);
+
+   for (int lev = 0; lev <= finest_level; lev++)
+   {
+      auto const& fact = EBFactory(lev);
+#if (AMREX_SPACEDIM == 2)
+      plotmf[lev].define(grids[lev],dmap[lev],9,0);
+      MultiFab::Copy(plotmf[lev], gradx[lev], 0, 0, 2, 0);
+      MultiFab::Copy(plotmf[lev], grady[lev], 0, 2, 2, 0);
+      MultiFab::Copy(plotmf[lev], gradeb[lev], 0, 4, 2, 0);
+      {
+         MultiFab temp_mf = fact.getBndryArea().ToMultiFab(0.0, 0.0);
+         MultiFab::Copy(plotmf[lev], temp_mf, 0, 6, 1, 0);
+      }
+      {
+         MultiFab temp_mf = fact.getBndryNormal().ToMultiFab(0.0, 0.0);
+         MultiFab::Copy(plotmf[lev], temp_mf, 0, 7, 2, 0);
+      }
+#endif
+   }
+
+   const std::string& pfname = amrex::Concatenate("drag_terms", m_nstep);
+
+   // Write the plotfile
+   amrex::WriteMultiLevelPlotfile(pfname, finest_level + 1, GetVecOfConstPtrs(plotmf),
+                                  {"dudx", "dvdx", "dudy", "dvdy", "dudn", "dvdn",
+                                   "barea", "bnrmx", "bnrmy"},
+                                  Geom(), m_cur_time, istep, refRatio());
+}
+*/
