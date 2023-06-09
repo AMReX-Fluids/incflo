e2fdadb6b18b2f15d2c47634b4dec2046881bccf
cgilet <cgilet@gmail.com>
2023-06-09 14:37:33 -0400

    Turn off merging nbhd printing.

--- src/redistribution/hydro_create_itracker.cpp
+++ src/redistribution/hydro_create_itracker.cpp
@@ -133,7 +133,7 @@ Redistribution::MakeITracker ( Box const& bx,
     });
 
 
-#if 1
+#if 0
     amrex::Print() << "\nInitial Cell Merging" << std::endl;
 
     amrex::ParallelFor(Box(itracker),
@@ -220,7 +220,7 @@ Redistribution::MakeITracker ( Box const& bx,
     amrex::Print() << std::endl;
 #endif
 
-#if 1
+#if 0
     amrex::Print() << "Post Update to Cell Merging" << std::endl;
 
     amrex::ParallelFor(Box(itracker),
