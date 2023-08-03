3334f586fb953065599f5da0d54b763d28cf4247
cgilet <cgilet@gmail.com>
2023-08-03 15:49:20 -0400

    Treat add NC correction to nb cell qhat. Only works with 1 nb

--- src/redistribution/hydro_redistribution.cpp
+++ src/redistribution/hydro_redistribution.cpp
@@ -379,20 +379,26 @@ void Redistribution::Apply ( Box const& bx, int ncomp,
                     // This will undo volume scaling that happens later in forming q-hat
                     delta_divU /= vfrac_old(i,j,k);
 
-                    if ( i==9 && j==8){
-                        Print()<<"NC DELTA DIVU "<<delta_divU
+                    if ( i==15 && j==12){
+                        Print()<<"1 DELTA DIVU "<<delta_divU
                                <<" "<<Ueb_dot_an
                             //<<" "<<Ueb_dot_an_new
                                <<" "<<delta_vol<<std::endl;
                     }
-                    if ( i==10 && j==8){
-                        Print()<<"NCN DELTA DIVU "<<delta_divU
+                    if ( i==16 && j==12){
+                        Print()<<"2 DELTA DIVU "<<delta_divU
                                <<" "<<Ueb_dot_an
                             //<<" "<<Ueb_dot_an_new
                                <<" "<<delta_vol<<std::endl;
-                        Print()<<"U "<<U_in(i,j,k,n)
-                               <<" "<<out(i,j,k,n)
-                               <<std::endl;
+                    }
+                    if ( i==16 && j==11){
+                        Print()<<"3 DELTA DIVU "<<delta_divU
+                               <<" "<<Ueb_dot_an
+                            //<<" "<<Ueb_dot_an_new
+                               <<" "<<delta_vol<<std::endl;
+                        // Print()<<"U "<<U_in(i,j,k,n)
+                        //        <<" "<<out(i,j,k,n)
+                        //        <<std::endl;
                     }
 
                     scratch(i,j,k,n) = U_in(i,j,k,n) + dt * dUdt_in(i,j,k,n)
@@ -477,70 +483,21 @@ void Redistribution::Apply ( Box const& bx, int ncomp,
                 }
                 else if ( vfrac_new(i,j,k) == 0.0 && vfrac_old(i,j,k) > 0. )
                 {
-                    // Newly covered cell and nbs
-                    
-                    // Create nbhd average U^n
-                    // Do we need U^pred-bar also?
-                    Real Ubar = U_in(i,j,k,n)*vfrac_old(i,j,k);
-                    Real vol_o  = vfrac_old(i,j,k);
-                    Real Upbar = 0.;
-                    Real vol_n  = 0.;
-                    for (int i_nbor = 1; i_nbor <= itr(i,j,k,0); i_nbor++)
+                    //
+                    // Newly covered cell
+                    // Give my correction to my neighbor
+                    //
+                    int nc = 1;
+                    if ( itr(i,j,k,0) > 1 ){
+                        nc = 3;
+                        Print()<<Dim3{i,j,k}<<" is NC and has "<<itr(i,j,k,0)<<" nbs"<<std::endl;
+                    }
                     {
-                        int ioff = map[0][itr(i,j,k,i_nbor)];
-                        int joff = map[1][itr(i,j,k,i_nbor)];
-                        int koff = (AMREX_SPACEDIM < 3) ? 0 : map[2][itr(i,j,k,i_nbor)];
+                        // No correction for me
+                        scratch(i,j,k,n) = U_in(i,j,k,n) + dt * dUdt_in(i,j,k,n);
 
-                        if ( Box(scratch).contains(Dim3{i+ioff,j+joff,k+koff}) )
+                        for (int i_nbor = 1; i_nbor <= itr(i,j,k,0); i_nbor++)
                         {
-                            Ubar += U_in(i+ioff,j+joff,k+koff,n)*vfrac_old(i+ioff,j+joff,k+koff);
-                            vol_o  += vfrac_old(i+ioff,j+joff,k+koff);
-                            Upbar += out(i+ioff,j+joff,k+koff,n)*vfrac_old(i+ioff,j+joff,k+koff);
-                            vol_n  += vfrac_new(i+ioff,j+joff,k+koff);
-                        }
-                    }
-                    Ubar /= vol_o;
-                    Upbar /= vol_n;
-
-                    Real delta_divU = 0.0;
-                    Real Ueb_dot_an = 0.0;
-                    Real delta_vol = (vfrac_new(i,j,k) - vfrac_old(i,j,k))/dt;
-
-                    {
-                        Ueb_dot_an =
-                            AMREX_D_TERM(  vel_eb_old(i,j,k,0)*bnorm_old(i,j,k,0) * dxinv[0],
-                                           + vel_eb_old(i,j,k,1)*bnorm_old(i,j,k,1) * dxinv[1],
-                                           + vel_eb_old(i,j,k,2)*bnorm_old(i,j,k,2) * dxinv[2] );
-                        Ueb_dot_an *= barea_old(i,j,k);
-
-                        delta_divU = (delta_vol - Ueb_dot_an) * Ubar;
-                    }
-
-                    // For the Corrector step
-                    if (vel_eb_new)
-                    {
-                        Real Ueb_dot_an_new =
-                            AMREX_D_TERM(  vel_eb_new(i,j,k,0)*bnorm_new(i,j,k,0) * dxinv[0],
-                                         + vel_eb_new(i,j,k,1)*bnorm_new(i,j,k,1) * dxinv[1],
-                                         + vel_eb_new(i,j,k,2)*bnorm_new(i,j,k,2) * dxinv[2] );
-                        Ueb_dot_an_new *= barea_new(i,j,k);
-
-                        // Use half of Ueb_dot_an and the full delta_vol
-                        // Needed to get 2D inputs_box_right to stay constant for covering
-                        delta_divU = Real(0.5) * (delta_divU + delta_vol*Upbar );
-                    }
-
-                    // This will undo volume scaling that happens later in forming q-hat
-                    delta_divU /= vfrac_old(i,j,k);
-
-                    scratch(i,j,k,n) = U_in(i,j,k,n) + dt * dUdt_in(i,j,k,n)
-                        + dt * delta_divU;
-
-                    //
-                    // Now correct nbs
-                    //
-                    for (int i_nbor = 1; i_nbor <= itr(i,j,k,0); i_nbor++)
-                    {
                         int ioff = map[0][itr(i,j,k,i_nbor)];
                         int joff = map[1][itr(i,j,k,i_nbor)];
                         int koff = (AMREX_SPACEDIM < 3) ? 0 : map[2][itr(i,j,k,i_nbor)];
@@ -551,38 +508,55 @@ void Redistribution::Apply ( Box const& bx, int ncomp,
                             int jj = j+joff;
                             int kk = k+koff;
 
-                            delta_vol = (vfrac_new(ii,jj,kk) - vfrac_old(ii,jj,kk))/dt;
+                            Real delta_divU = 0.0;
+                            Real Ueb_dot_an = 0.0;
+                            Real delta_vol = (vfrac_new(i,j,k) - vfrac_old(i,j,k))/dt;
 
                             Ueb_dot_an =
-                                AMREX_D_TERM(  vel_eb_old(ii,jj,kk,0)*bnorm_old(ii,jj,kk,0) * dxinv[0],
-                                             + vel_eb_old(ii,jj,kk,1)*bnorm_old(ii,jj,kk,1) * dxinv[1],
-                                             + vel_eb_old(ii,jj,kk,2)*bnorm_old(ii,jj,kk,2) * dxinv[2] );
-                            Ueb_dot_an *= barea_old(ii,jj,kk);
+                                AMREX_D_TERM(  vel_eb_old(i,j,k,0)*bnorm_old(i,j,k,0) * dxinv[0],
+                                             + vel_eb_old(i,j,k,1)*bnorm_old(i,j,k,1) * dxinv[1],
+                                             + vel_eb_old(i,j,k,2)*bnorm_old(i,j,k,2) * dxinv[2] );
+                            Ueb_dot_an *= barea_old(i,j,k);
 
-                            delta_divU = (delta_vol - Ueb_dot_an) * Ubar;
+                            delta_divU = (delta_vol - Ueb_dot_an) * U_in(ii,jj,kk,n);
+
 
                             // For the Corrector step
                             if (vel_eb_new)
                             {
-                                Real Ueb_dot_an_new =
-                                    AMREX_D_TERM(  vel_eb_new(ii,jj,kk,0)*bnorm_new(ii,jj,kk,0) * dxinv[0],
-                                                 + vel_eb_new(ii,jj,kk,1)*bnorm_new(ii,jj,kk,1) * dxinv[1],
-                                                 + vel_eb_new(ii,jj,kk,2)*bnorm_new(ii,jj,kk,2) * dxinv[2] );
-                                Ueb_dot_an_new *= barea_new(ii,jj,kk);
-
-                                delta_divU = Real(0.5) * (delta_divU
-                                                          + Upbar * (delta_vol - Ueb_dot_an_new));
+                                // Use half of Ueb_dot_an and the full delta_vol
+                                delta_divU = (Real(0.5)*Ueb_dot_an + delta_vol)*U_in(ii,jj,kk,n);
                             }
 
                             // This will undo volume scaling that happens later in forming q-hat
                             delta_divU /= vfrac_old(ii,jj,kk);
 
-                            scratch(ii,jj,kk,n) = U_in(ii,jj,kk,n) + dt * dUdt_in(ii,jj,kk,n)
-                                + dt * delta_divU;
+                            scratch(ii,jj,kk,n) += dt * delta_divU / itr(i,j,k,0);
+                            
+                            if ( ii==15 && jj==12){
+                                Print()<<"31 DELTA DIVU "<<delta_divU
+                                       <<" "<<Ueb_dot_an
+                                    //<<" "<<Ueb_dot_an_new
+                                       <<" "<<delta_vol<<std::endl;
+                            }
+                            if ( ii==16 && jj==12){
+                                Print()<<"32 DELTA DIVU "<<delta_divU
+                                       <<" "<<Ueb_dot_an
+                                    //<<" "<<Ueb_dot_an_new
+                                       <<" "<<delta_vol<<std::endl;
+                            }
+                            if ( ii==16 && jj==11){
+                                Print()<<"33 DELTA DIVU "<<delta_divU
+                                       <<" "<<Ueb_dot_an
+                                    //<<" "<<Ueb_dot_an_new
+                                       <<" "<<delta_vol<<std::endl;
+                                // Print()<<"U "<<U_in(i,j,k,n)
+                                //        <<" "<<out(i,j,k,n)
+                                //        <<std::endl;
+                            }
                         }
                     }
-
-
+                }
                 }
             });
         }
