6618dcb1e266e62342ae1cb19c9f9c584f370cb2
Ann Almgren <asalmgren@lbl.gov>
2023-08-03 15:48:21 -0700

    fix typo

--- src/incflo_apply_corrector.cpp
+++ src/incflo_apply_corrector.cpp
@@ -329,6 +329,7 @@ void incflo::ApplyCorrector()
                                 0, m_ntrac, refRatio(lev));
 #endif
     }
+    }
 
     // *************************************************************************************
     // Define the forcing terms to use in the final update (using half-time density)
