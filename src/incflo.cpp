2879d0e9539fca531b0e4d75a8f13f1ffe674b28
cgilet <cgilet@gmail.com>
2024-01-22 20:33:59 -0500

    Remove debugging Write

--- src/incflo.cpp
+++ src/incflo.cpp
@@ -224,7 +224,6 @@ void incflo::MakeNewLevelFromScratch (int lev, Real time, const BoxArray& new_gr
         prob_init_fluid(lev);
     }
     make_mixedBC_mask(lev, grids[lev], dmap[lev]);
-    Write(*m_mixedBC_mask[0],std::string("bcmask"));
 
 #ifdef AMREX_USE_EB
     macproj = std::make_unique<Hydro::MacProjector>(Geom(0,finest_level),
