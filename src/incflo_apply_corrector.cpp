5c609de510741cd75b35cb8a44b2ebcb5e844712
cgilet <cgilet@gmail.com>
2024-04-01 15:45:54 -0400

    Move tracer explicit update to it's own function

--- src/incflo_apply_corrector.cpp
+++ src/incflo_apply_corrector.cpp
@@ -165,7 +165,6 @@ void incflo::ApplyCorrector()
     // Define local variables for lambda to capture.
     // *************************************************************************************
     Real l_dt = m_dt;
-    int l_ntrac = (m_advect_tracer) ? m_ntrac : 0;
 
     // *************************************************************************************
     // Update density first
@@ -227,96 +226,8 @@ void incflo::ApplyCorrector()
     // *************************************************************************************
     if (m_advect_tracer)
     {
-        for (int lev = 0; lev <= finest_level; lev++)
-        {
-            auto& ld = *m_leveldata[lev];
-#ifdef _OPENMP
-#pragma omp parallel if (Gpu::notInLaunchRegion())
-#endif
-            for (MFIter mfi(ld.tracer,TilingIfNotGPU()); mfi.isValid(); ++mfi)
-            {
-                Box const& bx = mfi.tilebox();
-                Array4<Real const> const& tra_o   = ld.tracer_o.const_array(mfi);
-                Array4<Real const> const& rho_o   = ld.density_o.const_array(mfi);
-                Array4<Real      > const& tra     = ld.tracer.array(mfi);
-                Array4<Real const> const& rho     = ld.density.const_array(mfi);
-                Array4<Real const> const& dtdt_o  = ld.conv_tracer_o.const_array(mfi);
-                Array4<Real const> const& dtdt    = ld.conv_tracer.const_array(mfi);
-                Array4<Real const> const& tra_f   = (l_ntrac > 0) ? tra_forces[lev].const_array(mfi)
-                                                                : Array4<Real const>{};
-                auto iconserv = get_tracer_iconserv_device_ptr();
-
-                if (m_diff_type == DiffusionType::Explicit)
-                {
-                    Array4<Real const> const& laps_o = (l_ntrac > 0) ? ld.laps_o.const_array(mfi)
-                                                                     : Array4<Real const>{};
-                    Array4<Real const> const& laps   = (l_ntrac > 0) ? ld.laps.const_array(mfi)
-                                                                     : Array4<Real const>{};
-                    amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
-                    {
-                        for (int n = 0; n < l_ntrac; ++n)
-                        {
-                            if ( iconserv[n] ) {
-                                Real tra_new = rho_o(i,j,k)*tra_o(i,j,k,n) + l_dt * (
-                                     m_half*(  dtdt(i,j,k,n) + dtdt_o(i,j,k,n))
-                                    +m_half*(laps_o(i,j,k,n) +   laps(i,j,k,n))
-                                    +    tra_f(i,j,k,n) );
-
-                                tra(i,j,k,n) = tra_new / rho(i,j,k);
-                            } else {
-                                tra(i,j,k,n) = tra_o(i,j,k,n) + l_dt * (
-                                     m_half*(  dtdt(i,j,k,n) + dtdt_o(i,j,k,n))
-                                    +m_half*(laps_o(i,j,k,n) +   laps(i,j,k,n))
-                                    +    tra_f(i,j,k,n) );
-                            }
-                        }
-                    });
-                }
-                else if (m_diff_type == DiffusionType::Crank_Nicolson)
-                {
-                    Array4<Real const> const& laps_o = (l_ntrac > 0) ? ld.laps_o.const_array(mfi)
-                                                                     : Array4<Real const>{};
-                    amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
-                    {
-                        for (int n = 0; n < l_ntrac; ++n)
-                        {
-                            if ( iconserv[n] ) {
-                                Real tra_new = rho_o(i,j,k)*tra_o(i,j,k,n) + l_dt * (
-                                     m_half*(  dtdt(i,j,k,n) + dtdt_o(i,j,k,n))
-                                    +m_half*(laps_o(i,j,k,n)                  )
-                                    +    tra_f(i,j,k,n) );
-
-                                tra(i,j,k,n) = tra_new / rho(i,j,k);
-                            } else {
-                                tra(i,j,k,n) = tra_o(i,j,k,n) + l_dt * (
-                                     m_half*(  dtdt(i,j,k,n) + dtdt_o(i,j,k,n))
-                                    +m_half*(laps_o(i,j,k,n)                  )
-                                    +    tra_f(i,j,k,n) );
-                            }
-                    });
-                }
-                else if (m_diff_type == DiffusionType::Implicit)
-                {
-                    amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
-                    {
-                        for (int n = 0; n < l_ntrac; ++n)
-                        {
-                            if ( iconserv[n] ) {
-                                Real tra_new = rho_o(i,j,k)*tra_o(i,j,k,n) + l_dt * (
-                                     m_half*(  dtdt(i,j,k,n)+dtdt_o(i,j,k,n))
-                                    +      tra_f(i,j,k,n) );
-
-                                tra(i,j,k,n) = tra_new / rho(i,j,k);
-                            } else {
-                                tra(i,j,k,n) = tra_o(i,j,k,n) + l_dt * (
-                                     m_half*(  dtdt(i,j,k,n)+dtdt_o(i,j,k,n))
-                                    +      tra_f(i,j,k,n) );
-                            }
-                    });
-                }
-            } // mfi
-        } // lev
-    } // if (m_advect_tracer)
+        tracer_explicit_update_corrector(tra_forces);
+    }
 
     // *************************************************************************************
     // Solve diffusion equation for tracer
