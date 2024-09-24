125761dd6470f7cc1eda78077b0f44a104913c6e
Ann Almgren <asalmgren@lbl.gov>
2024-09-24 08:35:33 -0700

    query use_cc_proj

--- src/setup/init.cpp
+++ src/setup/init.cpp
@@ -72,6 +72,7 @@ void incflo::ReadParameters ()
         pp.query("godunov_use_forces_in_trans"      , m_godunov_use_forces_in_trans);
         pp.query("godunov_include_diff_in_forcing"  , m_godunov_include_diff_in_forcing);
         pp.query("use_mac_phi_in_godunov"           , m_use_mac_phi_in_godunov);
+        pp.query("use_cc_proj"                      , m_use_cc_proj);
 
         // What type of redistribution algorithm;
         // {NoRedist, FluxRedist, StateRedist}
