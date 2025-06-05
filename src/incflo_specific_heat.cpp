0dc9de30b7a555243d4c88fe8a3837cb421e6052
cgilet <cgilet@gmail.com>
2025-06-05 16:39:31 +0200

    Fix capture of class data member

--- src/incflo_specific_heat.cpp
+++ src/incflo_specific_heat.cpp
@@ -5,12 +5,13 @@ using namespace amrex;
 void incflo::compute_cp (int /*lev*/, MFIter& /*mfi*/, FArrayBox& cp) const
 {
     // Get leveldata if desired, e.g.
-    // Array4<Real const> const& rho   = density[lev]->const_array(mfi);
+    // Array4<Real const> const& rho   = m_leveldata[lev]->density.const_array(mfi);
 
     Box const& bx = cp.box();
     Array4<Real> const& cp_a = cp.array();
+    Real l_cp = m_cp;
     ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
-        cp_a(i,j,k) = m_cp;
+        cp_a(i,j,k) = l_cp;
     });
 }
